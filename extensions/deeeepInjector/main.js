374b8fe44f45c9b9ed5851efdd369035f04d95b0
nostopgmaming17 <noamshukrun2008@gmail.com>
2023-12-17 16:02:44 +0200

    Update main.js

--- extensions/deeeepInjector/main.js
+++ extensions/deeeepInjector/main.js
@@ -1,30 +1,24 @@
 var code = '(' + function() {
     const old = Function.prototype.bind;
-    let game;
+    let hookedname;
     const bind = function(...args) {
-        if (this == console.log) {return old.apply(this,args)};
-        if (Function.prototype.bind == old) {
-            return old.apply(this,args)
-        } else if (
-            args[0]&&
-            Object.prototype.hasOwnProperty.call(args[0],"currentScene")
-            ) {
-            console.warn("%c[TheJ Injector] Logged game object.","color: #ff4d36; font-size:200%");
-            game = args[0];
-            window.game = game;
+        if (args[0]&&Object.prototype.hasOwnProperty.call(args[0],"currentScene")) {
+            console.warn("%c[TheJ Injector] Logged game object.","color:#ff4d36;font-size:200%");
+            window.game = args[0];
             (()=>{
-                if (window.hookedname) return;
-                window.hookedname = true;
-                let SM = game.socketManager;
-                let olddr = SM.doRequest;
-                SM.doRequest = (...a) => {
-                    if (a[0] == 6 || a[0] == 8) {a[1]=a[1].replaceAll(/\\n/g,"\n").replaceAll(/\\t/g,"\t");}
+                if (hookedname) return;
+                hookedname = true;
+                const olddr = game.socketManager.doRequest;
+                game.socketManager.doRequest = (...a) => {
+                    if(a[0]==6||a[0]==8) {
+                        a[1] = a[1].replaceAll(/\\n/g,"\n").replaceAll(/\\t/g,"\t");
+                    }
                     return olddr(...a);
                 }
             })();
         }
         return old.apply(this,args)
-    };
+    }
     Function.prototype.bind = bind;
 } + ')();'
 var script = document.createElement('script')
